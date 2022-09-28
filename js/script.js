7e7b5052c62b3ffa5c2168c7eaafd909bc8a053d
Anna <58006989+Anna-Z-Krk@users.noreply.github.com>
2022-09-28 06:09:43 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,47 +1,52 @@
-const nav = document.querySelector('.nav');
-const navBtn = document.querySelector('.burger-btn');
-const navLink = document.querySelectorAll('.nav__item');
-const navBtnBars = document.querySelector('.burger-btn__bars')
-const allSections = document.querySelectorAll('.section')
-const footerYear = document.querySelector('.footer__year')
+const nav = document.querySelector(".nav");
+const navBtn = document.querySelector(".burger-btn");
+const navLink = document.querySelectorAll(".nav__item");
+const navBtnBars = document.querySelector(".burger-btn__bars");
+const allSections = document.querySelectorAll(".section");
+const footerYear = document.querySelector(".footer__year");
 
 const handleNav = () => {
-    nav.classList.toggle('nav--active');
-    navBtnBars.classList.remove('black-bars-color');
-    navLink.forEach(item => {
-        item.addEventListener('click', () => {
-            nav.classList.remove('nav--active');
-})
-    })
-    handleNavAmination()
-}
+  nav.classList.toggle("nav--active");
+  navBtnBars.classList.remove("black-bars-color");
+  navLink.forEach((item) => {
+    item.addEventListener("click", () => {
+      nav.classList.remove("nav--active");
+    });
+  });
+  handleNavAmination();
+};
 const handleNavAmination = () => {
-    let delayTime = 0;
+  let delayTime = 0;
 
-    navLink.forEach(item => {
-        item.classList.toggle('nav-items-animation')
-        item.style.animationDelay = "." + delayTime + "s";
-        delayTime++;
-    })
-}
+  navLink.forEach((item) => {
+    item.classList.toggle("nav-items-animation");
+    item.style.animationDelay = "." + delayTime + "s";
+    delayTime++;
+  });
+};
 const handleObserver = () => {
-    const currentSection = window.scrollY;
-    allSections.forEach(section => {
-        if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
-            navBtnBars.classList.add('black-bars-color')
-        } else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
-            navBtnBars.classList.remove('black-bars-color')
-        }
-    })
-}
-
+  const currentSection = window.scrollY;
+  allSections.forEach((section) => {
+    if (
+      section.classList.contains("white-section") &&
+      section.offsetTop <= currentSection + 60
+    ) {
+      navBtnBars.classList.add("black-bars-color");
+    } else if (
+      !section.classList.contains("white-section") &&
+      section.offsetTop <= currentSection + 60
+    ) {
+      navBtnBars.classList.remove("black-bars-color");
+    }
+  });
+};
 
 const handleCurrentYear = () => {
-    const year = (new Date).getFullYear();
-    footerYear.innerText = year;
-}
+  const year = new Date().getFullYear();
+  footerYear.innerText = year;
+};
 handleCurrentYear();
 
-navBtn.addEventListener('click', handleNav);
+navBtn.addEventListener("click", handleNav);
 
-window.addEventListener('scroll', handleObserver);
+window.addEventListener("scroll", handleObserver);
